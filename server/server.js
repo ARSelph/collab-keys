72dc31cf5de1ea6e0c389ec0a79c44bc5f9aa106
Adam Selph <adam@Adams-MBP.fios-router.home>
2022-08-31 12:12:49 -0400

    added gitignore

--- server/server.js
+++ server/server.js
@@ -3,17 +3,6 @@ const express = require('express');
 
 const app = express();
 
-const leaderList = [
-  {name: 'Anna', id: 'a0'},
-  {name: 'Ben', id: 'b0'},
-  {name: 'Clara', id: 'c0'},
-  {name: 'David', id: 'd0'},
-];
-
-app.get('/api/leaders', (req, res) => {
-  return res.status(200).send(leaderList);
-});
-
 // statically serve everything in the build folder on the route '/build'
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 // serve index.html on the route '/'
